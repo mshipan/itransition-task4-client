3bbe7c7d43001540d7af722c282802bd0b27c7ef
mshipan <mshipan657@gmail.com>
2024-07-11 21:54:09 +0600

    create login page design

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,34 +1,17 @@
 import userImg from "../../assets/images/user.png";
 
-const Register = () => {
+const Login = () => {
   return (
     <div className="flex h-screen min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img className="mx-auto h-20 w-auto" src={userImg} alt="User Logo" />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight capitalize text-gray-900">
-          create a new account
+          Sign in to your account
         </h2>
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form className="space-y-6" action="#" method="POST">
-          <div>
-            <label
-              htmlFor="fullName"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Full Name
-            </label>
-            <div className="mt-2">
-              <input
-                name="fullName"
-                type="text"
-                required
-                className="block w-full rounded-md border-0 px-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
           <div>
             <label
               htmlFor="email"
@@ -64,28 +47,11 @@ const Register = () => {
           </div>
 
           <div>
-            <label
-              htmlFor="cPassword"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Confirm Password
-            </label>
-            <div className="mt-2">
-              <input
-                name="cPassword"
-                type="password"
-                required
-                className="block w-full rounded-md border-0 px-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
             <button
               type="submit"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Sign Up
+              Sign In
             </button>
           </div>
         </form>
@@ -94,4 +60,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Register from "../pages/register/Register";
+import Login from "../pages/login/Login";
 
 const router = createBrowserRouter([
   {
     path: "/register",
     element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
